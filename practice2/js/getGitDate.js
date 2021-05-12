c2cd140eda32afa77d8ca004dc8c23722d3977e6
Matthew G. Hoskins <mgh@iastate.edu>
2021-05-12 12:33:01 -0500

    Using friends update time code

---
+++ practice2/js/getGitDate.js
@@ -0,0 +1,13 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        let repos = JSON.parse(this.responseText);
+        repos.forEach((repo)=>{
+            if(repo.name === "webWork"){
+                document.getElementById('gitTime').innerHTML = (`${repo.name}: <em>${new Date(repo.updated_at)}</em><br>`);
+            }
+        });
+    }
+};
+xhttp.open("GET", "https://api.github.com/users/mghoskins/repos", true);
+xhttp.send();
