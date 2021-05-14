6f911c8e2d0a1e13fa0ab67887b4d89435a96396
Matthew G. Hoskins <mgh@iastate.edu>
2021-05-14 10:51:36 -0500

    Current work on caledar dev

---
+++ practice2/js/moment.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
+
+export const MyCalendar = props => (
+    <div style="height: 500px;">
+        <Calendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }}
+        />
+    </div>
+)
+
+const Footer = () => {
+    return(
+        <h2>THIS IS A FOOTER</h2>
+    )
+}
+export default Footer
