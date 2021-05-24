7f6630f60b7b6599d50e3fffa7b15c71a1a07e70
Matthew G. Hoskins <mgh@iastate.edu>
2021-05-24 10:12:13 -0500

    Method 2 - notingh

--- practice2/js/bottomInfo.js
+++ practice2/js/bottomInfo.js
@@ -1,27 +1,8 @@
-import css from 'css/main.css'
-import js from './getGitDate';
+const div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
 
-document.write(Test1);
-
-document.write(
-    <div className="bottomInfo">
-        <div className="leftSide">
-            <div className="infoHeader"><b>General Site Information</b></div>
-            <div className="infoText">
-                <p className="infoTextHeader">Last Website Update: </p>
-                <script src="js/getGitDate.js"></script>
-                <div id="gitTime"></div>
-                <p className="infoTextHeader">Author: </p>
-                <p>Not Willing to Say at This Time</p>
-            </div>
-        </div>
-
-        <div className="centerSide">
-            <div className="infoHeader"><b>Links</b></div>
-        </div>
-
-        <div className="rightSide">
-            <div className="infoHeader"><b>TODO</b></div>
-        </div>
-    </div>
-);
+document.body.appendChild(div);
