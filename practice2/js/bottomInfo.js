1b942572bc8a67832d0e47ce7cac693c43a8c452
Matthew G. Hoskins <mgh@iastate.edu>
2021-05-24 10:31:05 -0500

    Documentation

--- practice2/js/bottomInfo.js
+++ practice2/js/bottomInfo.js
@@ -6,3 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.body.appendChild(div);
 }, true);
+
+// This is not the final product
+// This is testing to figure out how to setup a div that will appear on multiple html pages to take up less space and code
